914b98d74910898945b1f3fa9f5316348853f842
Remy CASTRO <remycastro27@icloud.com>
2023-05-30 15:05:08 +0200

    Implement firebase and add main activity

---
+++ app/src/main/java/fr/upjv/MainActivity.java
@@ -0,0 +1,16 @@
+package fr.upjv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.upjv.miage_2023_android_projet.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
